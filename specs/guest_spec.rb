1fbf0fec34724eedf9acf5b020fd5600afdd7739
Jamie Reid <wjreid85.jr@gmail.com>
2018-11-30 12:37:01 +0000

    Create guest class and add guest name passing test.

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -9,7 +9,10 @@ require_relative("../guest")
 class TestGuest < MiniTest::Test
 
 
-
+  def test_guest_has_a_name()
+    guest = Guest.new("Jim Morrison")
+    assert_equal("Jim Morrison", guest.guest_name)
+  end
 
 
 
