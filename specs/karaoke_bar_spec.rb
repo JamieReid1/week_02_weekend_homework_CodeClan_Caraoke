fbb6a20a52e2b8cc7eacc99c043754768e24923b
Jamie Reid <wjreid85.jr@gmail.com>
2018-11-30 12:20:27 +0000

    Add room name passing test.

--- specs/karaoke_bar_spec.rb
+++ specs/karaoke_bar_spec.rb
@@ -9,7 +9,7 @@ require_relative("../karaoke_bar")
 class TestKaraokeBar < MiniTest::Test
 
 
-  def test_karaoke_bar_has_name()
+  def test_karaoke_bar_has_a_name()
     karaoke_bar = KaraokeBar.new("Singers Anonymous")
     assert_equal("Singers Anonymous", karaoke_bar.bar_name)
   end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -9,6 +9,11 @@ require_relative("../room")
 class TestRoom < MiniTest::Test
 
 
+  def test_room_has_a_name()
+    room = Room.new("Glam Rock Room")
+    assert_equal("Glam Rock Room", room.room_name)
+  end
+
 
 
 
