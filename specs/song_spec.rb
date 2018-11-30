198cec90af825e2b769cac2fdad7623fd6692a23
Jamie Reid <wjreid85.jr@gmail.com>
2018-11-30 13:01:42 +0000

    Add song class and song name passing test.

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -9,7 +9,10 @@ require_relative("../song")
 class TestSong < MiniTest::Test
 
 
-
+  def test_song_has_a_name()
+    song = Song.new("Tequila")
+    assert_equal("Tequila", song.song_name)
+  end
 
 
 
